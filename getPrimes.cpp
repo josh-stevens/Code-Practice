99345a3047901c8734aaa9a295d3f2e65576253f
josh-stevens <jcstevens12@outlook.com>
2015-06-11 14:43:23 -0500

    Took a different approach
    First, separated the getPrimes function from the main function. Took
    user input in the main function and called the getPrimes function based
    on that. Then, completely reworked the function to properly spit out
    prime numbers. Works like a charm.

--- getPrimes.cpp
+++ getPrimes.cpp
@@ -1,27 +1,70 @@
 #include <stdio.h>
-#include <iostream.h>
+#include <iostream>
+using namespace std;
 
-int main() {
-	void getPrimes(limit) {
+void getPrimes(int limit) {
 
-		int divisors = 0;
+	if (limit <= 1) {
 
-		for (i = 2; i<limit; i++) {
+		cout << "No primes" << endl;
 
-			for (j = 2; j<i; j++) {
+	}
+
+	else {
+
+		// 2 is the only even prime, and since we know the limit
+		// is at least 2, we can go ahead and print it out, and
+		// then we are just dealing with odd numbers
+
+		cout << 2 << " ";
+
+		// Starting with 3, count up and test each odd number (i) for 
+		// being a prime until the program reaches a number greater
+		// than the limit
+
+		for (int i = 3; i <= limit; i += 2) {
+
+			// Assume the number is a prime, then attempt to disprove it
+
+			bool pFlag = true;
+
+			// Since we are counting odd numbers only, nothing will be
+			// divisible by 2, so starting checking for divisibility with
+			// 3 and test every single number (j) until (i) is reached
+
+			for (int j = 3; j < i; j++) {
+
+				// If (i) is divisible by any number (j) with no remainder,
+				// then (i) is not a prime
 
 				if (i%j == 0) {
 
-					divisors++;
+					pFlag = false;
 
 				}
+
 			}
 
-			if (divisors == 1) {
+			// If the loop doesn't disprove (i) as a prime, then it must
+			// be a prime and should be printed to the screen.
 
-				cout << i;
+			if (pFlag) {
+				
+				cout << i << " ";
 
 			}
 		}
 	}
+	
+}
+
+int main() {
+
+	int x;
+
+	cout << "Count primes up to what number? ";
+	cin >> x;
+	getPrimes(x);
+	return 0;
+
 }
