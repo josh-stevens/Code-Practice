c00a819d3aea6bb5245e01f0f7ac5501dea8825a
josh-stevens <jcstevens12@outlook.com>
2015-06-15 14:37:12 -0500

    An example recursive function

---
+++ findSolution.js
@@ -0,0 +1,19 @@
+// RECURSION EXAMPLE
+// This function will find whatever combination of +5 or *3 it needs
+// to reach the target number.
+
+function findSolution(target) {
+  function find(start, history) {
+    if (start == target)
+      return history;
+    else if (start > target)
+      return null;
+    else
+      return find(start + 5, "(" + history + " + 5)") ||
+             find(start * 3, "(" + history + " * 3)");
+  }
+  return find(1, "1");
+}
+
+console.log(findSolution(24));
+// → (((1 * 3) + 5) * 3)
